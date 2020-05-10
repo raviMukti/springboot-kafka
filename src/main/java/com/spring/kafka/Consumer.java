1dffd09d50cc70e824f30ed0a60802b7524f40fa
raviMukti <shinjiravi@gmail.com>
2020-05-10 13:07:17 +0700

    Finishing tutorial

---
+++ src/main/java/com/spring/kafka/Consumer.java
@@ -0,0 +1,35 @@
+package com.spring.kafka;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Properties;
+
+public class Consumer {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, "test");
+        properties.setProperty(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        properties.setProperty(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        consumer.subscribe(Arrays.asList("contoh-topic"));
+
+        while (true){
+            ConsumerRecords<String, String> records =consumer.poll(Duration.ofMillis(100));
+            for (ConsumerRecord<String, String> record : records){
+                System.out.println("Receive data : " + record.value());
+            }
+        }
+    }
+}
